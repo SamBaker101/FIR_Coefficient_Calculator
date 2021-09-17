d45f7f4381297a5f5baf078722a746d99302f7a8
Sam Baker <Sam.Baker101@gmail.com>
2021-09-17 15:23:34 -0400

    Cleaned Main, setting up plotting
    Cleaned main function
    
    Began setting up plotting - in progress

--- main.c
+++ main.c
@@ -15,26 +15,34 @@ struct attributes {
 };
 
 
+//Utility functions
 void get_attributes(struct attributes*);
-double* low_pass(struct attributes*, double*);
-
 double* normalize(struct attributes*, double*);
 void print_coeffs(struct attributes*, double*);
+void print_attr(struct attributes*);
+void generate_impulse_file(struct attributes*, double*);
+void plot_impulse(struct attributes*, double*);
+
+
+//Calculator Functions
+double* low_pass(struct attributes*, double*);
 
 
 int main() {
-	struct attributes instance;
 	printf("Welcome \n\n");
 
+	struct attributes instance;
 	get_attributes(&instance);
-
-	printf("\nSample Rate: %d, Taps: %d \n", 
-		instance.sample_freq, instance.taps);
-
 	double* impulse_resp = (double*)malloc((instance.taps+1)*sizeof(double));
+
+	print_attr(&instance);
+	
 	impulse_resp = low_pass(&instance, impulse_resp);
 	impulse_resp = normalize(&instance, impulse_resp);
 	print_coeffs(&instance, impulse_resp);
+
+	generate_impulse_file(&instance, impulse_resp);
+	plot_impulse(&instance, impulse_resp);
 	
 }
 
@@ -50,6 +58,8 @@ void get_attributes(struct attributes* instance){
 	scanf("%d", &(instance->taps));
 }
 
+
+//Calculates coefficients using SINC function and shifts impulse response
 double* low_pass(struct attributes* instance, double* h){
 	int cutoff;
 	double temp;
@@ -69,6 +79,7 @@ double* low_pass(struct attributes* instance, double* h){
 }
 
 
+//Normalize Coefficient Values
 double* normalize(struct attributes* instance, double* h){
 	double sum = 0;
 
@@ -85,10 +96,58 @@ double* normalize(struct attributes* instance, double* h){
 }
 
 
+//Print Coefficients to Console
 void print_coeffs(struct attributes* instance, double* h){
 	printf("\n Coefficients: \n \n");
 	for (int i = 0; i < instance->taps; i++){
 		printf("%d   %1.20f \n",i, h[i]);
+		fflush(stdout);
 	}
 	printf("\n");
+}
+
+
+//Print Attributes to Console
+void print_attr(struct attributes* instance){
+	printf("\nSample Rate: %d, Taps: %d \n\n", 
+		instance->sample_freq, instance->taps);
+}
+	
+
+//Generates a data file containing the impulse response of the system for plotting
+void generate_impulse_file(struct attributes* instance, double* h){
+	FILE* file_pointer;
+
+	file_pointer = fopen("data/impulse.txt", "w");
+
+	if(file_pointer == NULL){
+		printf("Error generating impulse.dat");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < instance->taps; i++){
+		fprintf(file_pointer, "%f\t %f\n", i*(1.0/instance->sample_freq), h[i]);
+	}
+
+
+	fclose(file_pointer);
+	printf("File Generated \n");
+	fflush(stdout);
+}
+
+void plot_impulse(struct attributes* instance, double* h){
+	FILE* gnuplot = popen("gnuplot", "w");
+	char* filename = "data/impulse.txt";
+
+	if(gnuplot == NULL){
+		printf("Error opening gnuplot");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(gnuplot, "plot \"%s\" t 'Price' w lp\n", filename);
+	fflush(gnuplot);
+	fprintf(stdout, "Click Ctrl+d to quit...\n");
+	getchar();
+
+	pclose(gnuplot);
 }
